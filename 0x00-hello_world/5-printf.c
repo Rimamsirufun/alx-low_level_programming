206a4ad4d589523326481dd4fba6c6c927dbaee3
Rimamsirufun <rimamsirufun@gmail.com>
2023-02-17 11:31:52 -0800

    TASK 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main is void
+ *
+ * return : 0 when successful executed
+ */ 
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,""\n");
+	return (0);
+}	
