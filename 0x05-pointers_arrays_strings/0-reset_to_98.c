d6d71ddfd213450defe93708cb466dbd6041a93e
Rimamsirufun <rimamsirufun@gmail.com>
2023-02-28 19:52:55 -0800

    98 battery street

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * reset_to_98  
- * Value points to 98
+ * reset_to_98 - Takes a pointer to an int and updates  
+ * the value it points to 98
  *
  * @n: An integer input pointer
  *
- * Return : 0
+ * Return : nothing
  */
 void reset_to_98(int *n)
 {
